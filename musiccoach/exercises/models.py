ff7eccf285c099e1800353ae228a5f851fc76ca2
Charlie9009 <charlie.gustafsson90@gmail.com>
2025-09-30 21:44:19 +0200

    MODELS.PY: Add model for exercises

--- musiccoach/exercises/models.py
+++ musiccoach/exercises/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Exercise(models.Model):
+    EXERCISE_TYPE = [
+        ('WARM', 'Warm Up')
+        ('SCAL', 'Scales')
+        ('SONG', 'Song Training')
+        ('TECH', 'Technique')
+        ('OTHE', 'Other')
+    ]
+
+    date = models.DateField(auto_now_add=True)
+    type = models.CharField(max_length=4, choices=EXERCISE_TYPE)
+    description = models.TextField()
+    time = models.PositiveIntegerField()
+    notes = models.CharField(blank=True)
+
+    def __str__(self):
+        return f"{self.get_type_display()} ({self.time} min) - {self.date}"
