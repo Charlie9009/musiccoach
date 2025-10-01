5b9cc40b1c62d4c86a89616bf2705ebbcb919f13
Charlie9009 <charlie.gustafsson90@gmail.com>
2025-10-01 20:59:38 +0200

    ADMIN.PY: Add functionality for admin to add exercises

--- musiccoach/exercises/admin.py
+++ musiccoach/exercises/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Exercise
 
 # Register your models here.
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'get_type_display', 'time')
+    list_filter = ('type', 'date')
+    search_fields = ('description', 'notes')
