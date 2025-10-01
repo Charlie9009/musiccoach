b462bb29d425aa60139c6c4a60f5407cd24e61f8
Charlie9009 <charlie.gustafsson90@gmail.com>
2025-10-01 21:39:22 +0200

    VIEWS.PY: Add views for exercises

--- musiccoach/exercises/views.py
+++ musiccoach/exercises/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Exercise
+from .forms import ExerciseForm
+from datetime import date, timedelta
 from django.http import HttpResponse
 
 
-def exercises(request):
-    return HttpResponse("Hello world!")
+def index(request):
+    today = date.today()
+    latest_week = today - timedelta(days=7)
+    exercises = Exercise.objects.filter(date__gte=latest_week).order_by('-date')
+    total_time = sum([o.time for o in exercises])
+    return render(request, 'exercises/index.html', {
+        'exercises': exercises,
+        'total_time': total_time
+    })
+
+
+def create_exercise(request):
+    if request.method == 'POST':
+        form = ExerciseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ExerciseForm()
+
+    return render(request, 'exercises/create_exercise.html', {'form': form})
+
