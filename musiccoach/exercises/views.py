f4236ee49858383ae8da180e1ec2517085ca0c90
Charlie9009 <charlie.gustafsson90@gmail.com>
2025-10-02 21:56:56 +0200

    VIEWS.PY: Add functionality for django charts for the exercises

--- musiccoach/exercises/views.py
+++ musiccoach/exercises/views.py
@@ -3,16 +3,26 @@ from .models import Exercise
 from .forms import ExerciseForm
 from datetime import date, timedelta
 from django.http import HttpResponse
+from django.db.models import Sum
+from django.utils import timezone
 
 
 def index(request):
     today = date.today()
     latest_week = today - timedelta(days=7)
-    exercises = Exercise.objects.filter(date__gte=latest_week).order_by('-date')
-    total_time = sum([o.time for o in exercises])
-    return render(request, 'exercises/index.html', {
-        'exercises': exercises,
-        'total_time': total_time
+    exercises = Exercise.objects.filter(date__gte=latest_week).order_by('date')
+
+    # group minutes by date
+    chart_labels = [ex.date.strftime("%a") for ex in exercises]
+    chart_data = [ex.time for ex in exercises]
+
+    total_time = sum(chart_data)
+
+    return render(request, "exercises/index.html", {
+        "exercises": exercises,
+        "total_time": total_time,
+        "chart_labels": chart_labels,
+        "chart_data": chart_data,
     })
 
 
@@ -26,4 +36,3 @@ def create_exercise(request):
         form = ExerciseForm()
 
     return render(request, 'exercises/create_exercise.html', {'form': form})
-
